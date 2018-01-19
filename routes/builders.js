8106e13375e20b0ae5cd7889bda6ee44d53be2e3
Tanya Kasa <kasatanya1@gmail.com>
2018-01-19 22:13:39 +0000

    route for function findByName()

--- routes/builders.js
+++ routes/builders.js
@@ -4,8 +4,20 @@
 var express = require('express');
 var router = express.Router();
 var builders = require('../controllers/buildersControllers');
-var passport = require('passport');
+//var passport = require('passport');
 
+/*it is a middleware function that gives the value of that id, it is a forth argument */
+exports.function = router.param('id', function(req, res, next,id){
+    var g = _.find(builders, {id:id});
+    if(g){
+        req.g = g;
+        next();
+    }else{
+        res.send();
+    }
+});
+
+router.get('/list', builders.list);
 
 router.get('/registration',builders.showRegistrationPage);
 
@@ -13,10 +25,15 @@ router.post('/registration',builders.save);
 
 router.get('/profile/:id',builders.show);
 
-router.get('/profile/:name', builders.findByName);
+router.post('/show', builders.findByName);
 
-router.get('/list', builders.list);
+router.get('/edit/:id', builders.edit);// Edit builder
+
+router.get('/show/:id', builders.show);//get single builder by id
+
+router.post('/update/:id', builders.update);
+
+router.post('/delete/:id', builders.delete);
 
 module.exports = router;
 
-console.log(true? "true" : "false");
