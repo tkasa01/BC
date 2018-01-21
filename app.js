f7f38dc6bd3287fd95e52f9de32749a4534cc911
Tanya Kasa <kasatanya1@gmail.com>
2018-01-21 23:22:59 +0000

    lodash was added

--- app.js
+++ app.js
@@ -5,15 +5,23 @@ var favicon = require('serve-favicon');
 var session = require('express-session');
 var shortid = require('shortid');
 var mongoose = require('mongoose');
-var passport = require('passport');
 var methodOverride = require('method-override');
-var validator = require('express-validator');
+
+var validator = require('validator');
+var _ = require('lodash');
+var jwt = require('jsonwebtoken');
+var passport = require('passport');
+var passportJWT = require('passport-jwt');
+var ExtractJwt = passportJWT.ExtractJwt;
+var JwtStrategy = passportJWT.Strategy;
+
 var flash = require('connect-flash');
 var api = require('./api/router');
 var imagesDownload = require('./api/mongoose');
 //var error = require('./error.js');
 require('./middlewareApp')(app);
-require('./config/passport')(app, passport);
+
+//require('./config/passport')(app, passport);
 //require('./db');
 
 mongoose.Promise = global.Promise;
@@ -27,6 +35,13 @@ console.log('listen on port 3000' + db);
 //app.locals.info = 'flash';
 app.locals.siteTitle = 'Building Company';
 
+/*it is a middleware function that gives the value of that id, it is a forth argument */
+app.param('id', function(req,res, next,id){
+        console.log(id);
+        next();
+});
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -37,7 +52,7 @@ app.use('/', api);
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(methodOverride());
 app.use(session({
-    secret: 'foo',
+    secret: 'foolala',
     saveUninitialized: true,
     resave: true,
     //store: new  MongoStore({
@@ -46,52 +61,30 @@ app.use(session({
    // }),
     cookie: {maxAge: 180*60*1000} //2 hours
 }));
+
+//================================================================//
+//Express Messages Middleware
+//app.use(function (req, res, next) {
+  //  res.locals.sessionFlash = req.sessionFlash;
+    //res.locals.messages = require('express-messages')(req, res);
+   // delete req.locals.session.sessionFlash;
+   // next();
+//});
+
+//===================================================================
+
 app.use(passport.initialize());
 app.use(passport.session());//persistent login session
-app.use(flash());//use connect-flash for flash messages stored in session
-
-
-// Custom flash middleware -- from Ethan Brown's book, 'Web Development with Node & Express'
-app.use(function (req, res, next){
-     res.locals.message = req.flash();
-   next();
-});
-
-//Express Validation Middleware
-app.use(validator
-({errorFormatter: function (param, msg, value) {
-        var namespace = param.split('.'),
-            root = namespace.shift(),
-            formParam = root;
-        while(namespace.length){
-            formParam += '[' + namespace.shift() + ']';
-        }
-        return{
-            param: formParam,
-            msg: msg,
-            value: value
-        };
-    }})
-);
-
-// Route that creates a flash message using custom middleware
-/*
-app.all('/session-flash', function( req, res ) {
-    req.session.sessionFlash = {
-        type: 'success',
-        message: 'This is a flash message using custom middleware and express-session.'
-    };
-    res.redirect(301, '/');
-});
-*/
-//app.use(error());
-
 
 require('./models/Builder');
 var Builder = mongoose.model('Builder');
 
 require('./models/Customer');
 var Customer = mongoose.model('Customer');
+
+require('./models/Post');
+var Post = mongoose.model('Post');
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -100,10 +93,16 @@ app.use(function(req, res, next) {
   next(err);
 });
 
+app.get('/favicon.ico', function(req, res) {
+    res.status(204);
+});
+
 app.use(function(req,res,next){
-    req.errors = {};
+    req.messages = [];
+    req.errors = [];
     next();
 });
+
 
 // error handler
 app.use(function(err, req, res, next) {
