1eabb9a8613ec3bd070937471d7af634f8215371
Tanya Kasa <kasatanya1@gmail.com>
2018-01-23 00:21:02 +0000

    user:req.user was added

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,9 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', {
-                       siteTitle: 'Builders Company',
-                       pageTitle: 'Home page'
+                        siteTitle: 'Builders Company',
+                        pageTitle: 'Home page',
+                        user: req.user
        });
 });
 
