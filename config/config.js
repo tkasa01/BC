edae8af6ab148b54a92e6c13316d521597c2bbb2
Tanya Kasa <kasatanya1@gmail.com>
2018-01-23 00:09:21 +0000

    secret was created

--- config/config.js
+++ config/config.js
@@ -1,4 +1,51 @@
 /**
  * Created by tkasa on 09/01/2018.
  */
+module.exports= {
+    secret: 'lots of power in one action',
+    expTime: 840000
+    };
+
+/*
+var _ = require ('lodash');
+
+//default configuration for this api
+var config = {
+    dev: 'development',
+    test: 'testing',
+    prod: 'production',
+    port: process.env.PORT || 3000  //this is port what i am using
+};
+
+// check if the NODE_ENV was set, if isn't, then set it to dev
+process.env.NODE_ENV = process.env.NODE_ENV || config.dev;
+
+config.env = process.env.NODE_ENV;
+
+var envConfig;
+
+try{
+    envConfig = require('./'+ config.env);
+    envConfig = envConfig || {};//if require file doesn't exist
+}catch (e){
+    envConfig = {};
+}
+
+module.exports = _.merge(config, envConfig);
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
