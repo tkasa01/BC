b2f8651143dbf6cb4d17aae42df7b46b73adb52a
Tanya Kasa <kasatanya1@gmail.com>
2018-01-23 00:19:02 +0000

    schema for review was added

--- models/Post.js
+++ models/Post.js
@@ -11,11 +11,36 @@ var PostSchema = new Schema({
         type:String,
         required: true
        },
+    create:{
+        type: Date
+    },
+    update:{
+        type: Date,
+        default: Date.now()
+    },
     author:{
         type:Schema.Types.ObjectId,
-        ref: 'customer',
+        ref: 'customer',  //from customer collection
         required: true
+    },
+    builder:{
+        type: String,
+        default: null //if !null then its a review
     }
 });
 
-module.exports = mongoose.model('Post', PostSchema);
+//this middleware make sure that this post is created from this author
+PostSchema.pre('save', function (next) {
+    this.text = post(this.author); //slygify in the video
+    next();
+});
+
+function post(text){
+    return text.toString().toLowerCase()
+        .replace(/\s+/g,'-') //replace spaces with -
+        .replace(/[^(\w\-]+/g,'') //remove all non world chars
+        .replace(/^-+/,'') //trim start of text
+        .replace(/-+$/,''); // trim end of text
+}
+
+var Post = module.exports = mongoose.model('Post', PostSchema);
