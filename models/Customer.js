ed6c0a6d39d136a31fc2f270cad142d44d8aa433
Tanya Kasa <kasatanya1@gmail.com>
2018-01-23 00:16:58 +0000

    bcrypt module  and middleware pre save was added

--- models/Customer.js
+++ models/Customer.js
@@ -2,6 +2,7 @@
  * Created by tkasa on 12/12/2017.
  */
 var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
 
 var CustomerSchema = new mongoose.Schema({
     title: {
@@ -10,19 +11,54 @@ var CustomerSchema = new mongoose.Schema({
     firstname: {type:String,required: true},
     lastname:{type: String,required: true},
     dob: {type: Date},
-    emai: {type: String, trim: true},
+    email: {type: String, trim: true},
     phonenumber: {type: String,required: true, length: 11},
     address: {
                 state:{type: String},
                 city:{type: String},
                 postcode:{type:Number, require: true}
              },
-    password: {type: String, required:true},
-    confpassword: {type: String, required:true},
+    hash: {type: String, required:true},
     updated: {
         type: Date,
         default: Date.now
     }
+    /*
+    post: [{ref:'post',    //inverse association of denormalisation data
+            type: Schema.Types.ObjectId
+           }]*/
 });
 
- module.exports = mongoose.model('Customer', CustomerSchema);
+CustomerSchema.pre('save', function (next) {
+    if(!this.isModified('password'))return next();
+    this.password = this.encryptPassword(this.password);
+    next();
+});
+
+CustomerSchema.pre('save', function (next) {
+    var self = this;
+    Customer.find({email : self.email}, function (err, docs) {
+        if (!docs.length){
+            next();
+        }else{
+            console.log('user exists: ', self.email);
+            next(new Error("User exists!"));
+        }
+    });
+}) ;
+
+CustomerSchema.methods = {
+    authenticate:function (plaintextPassword) {
+        return bcrypt.compareSync(plaintextPassword, text.password);
+    },
+    encryptPassword: function(plaintextPassword){
+        if(!plaintextPassword){
+            return ''
+        }else {
+            var salt = bcrypt.genSaltSync(8);
+            return bcrypt.hashSync(plaintextPassword, salt);
+        }
+    }
+};
+
+var Customer = module.exports = mongoose.model('Customer', CustomerSchema);
