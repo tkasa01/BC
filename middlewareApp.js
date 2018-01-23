19fadd19de2297d20f085fb670002a17740da9ee
Tanya Kasa <kasatanya1@gmail.com>
2018-01-23 00:24:13 +0000

    store for jwt was added

--- middlewareApp.js
+++ middlewareApp.js
@@ -1,6 +1,8 @@
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
 var cookieParser = require('cookie-parser');
+var store = require('store');
+var auth = require('./auth');
 
 module.exports = function (app) {
     app.use(morgan('dev'));
@@ -10,9 +12,14 @@ module.exports = function (app) {
 
     //for jwt token
     app.use(function(req,res,next){
-        if(true){
-            //req.user = decode
+        if(store.get('jwt')){
+            auth.checkAuth(store.get('jwt')).then(function(user){
+                req.user = user;
+            },function(err){
+                //handle error
+            })
         }else{
+            console.log('I am not log');
             req.user = null;
         }
         next();
