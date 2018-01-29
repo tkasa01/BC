4de4b0bd945e8997c08ad66979152730d18f814f
Tanya Kasa <kasatanya1@gmail.com>
2018-01-29 16:28:15 +0000

    role was added

--- middlewareApp.js
+++ middlewareApp.js
@@ -3,6 +3,7 @@ var morgan = require('morgan');
 var cookieParser = require('cookie-parser');
 var store = require('store');
 var auth = require('./auth');
+var Builder = require('./models/Builder');
 
 module.exports = function (app) {
     app.use(morgan('dev'));
@@ -10,18 +11,26 @@ module.exports = function (app) {
     app.use(bodyParser.json());
     app.use(cookieParser());
 
+
     //for jwt token
     app.use(function(req,res,next){
         if(store.get('jwt')){
             auth.checkAuth(store.get('jwt')).then(function(user){
+                console.log('I am log in');
                 req.user = user;
+                req.role = user.user.role;
             },function(err){
                 //handle error
             })
         }else{
             console.log('I am not log');
             req.user = null;
+            req.role = "guest";
         }
         next();
     })
 };
+
+
+
+
