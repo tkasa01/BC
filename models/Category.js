ffddc812682cd58f04e8a98d2a9d82d3aea5be45
Tanya Kasa <kasatanya1@gmail.com>
2018-01-29 16:35:00 +0000

    code was cleaned

--- models/Category.js
+++ models/Category.js
@@ -2,9 +2,10 @@ const mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var CategorySchema = new Schema({
-    name: {type:String,
-    required: true,
-    unique: true }
-});
+    name: {
+        type:String,
+        required: true,
+        unique: true }
+    });
 
 module.exports = mongoose.model('Category', CategorySchema);
