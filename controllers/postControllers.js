1cc23d0c35a6bbbbe7528ddbc302d9ee348e39e0
Tanya Kasa <kasatanya1@gmail.com>
2018-02-04 22:27:05 +0000

    save function was created

--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -4,12 +4,11 @@
 var mongoose = require('mongoose');
 var Post = require('../models/Post');
 var Customer = require('../models/Customer');
-var promise = require('promise');
 
 var postController = {};
 
 postController.list = function (req, res) {
-    console.log('heiiii');
+
     Post.find({}).populate('author').exec(function (err, posts) {
         if(err){
             res.send(err);
@@ -17,50 +16,53 @@ postController.list = function (req, res) {
             res.render('./posts/posts', {
                 pageTitle: 'Recent posts from customers',
                 posts:posts,
-                user:req.user
+                user:req.user,
+                author: req.author
             });
         }
     });
 };
 
-postController.create = function (req, res) {
-    console.log('create function');
-    res.render('./customers/profile',{
-        pageTitle: 'Registration Page for Customers',
-        user: req.user,
-        errors: global.errors
-    });
-    global.errors = '';
-
-};
-
-postController.savePost = function(req, res){
+postController.savePost = function(req, res, next) {
     //var postSorted = post.object('Post').sorted('timestamp', true);
-    var post = new Post(req.body);
+    var post = new Post({content: req.body.content, title: req.body.title,  author: req.body.author});
+    console.log(post);
     post.save(function (err, post) {
         if (err) {
             res.send(err);
         } else {
-            console.log(post);
-            res.render('./posts/posts');
-        }
-    });
+               // console.log("saved" + post);
+                res.redirect('/posts/posts');
+            }
+        });
 };
 
 /*
+ Post.findById(author).populate('author').exec(function (err, author) {
+ if (err) {
+ res.send(err);
+ }
+ else {
+ // console.log("saved" + post);
+ res.redirect('/posts/posts' + author);
+ }
+ });
+ */
+
+
 postController.params = function(req, res, next, id){
-    Post.findById(id).exec(function(err, post){
+    Post.findById(id).populate('author').exec(function(err, post, author){
         if(!post){
             next(new Error('No post found from this id'));
         }else{
+            req.author = author;
             req.post = post;
             next();
         }
     })
 };
 
-
-
+/*
 postController.update = function (req, res) {
         var post = req.post;
         var newPost = req.body;
