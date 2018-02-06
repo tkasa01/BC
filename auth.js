c8ad0690d6ddc03dae3108e5538e6da53f542bdd
Tanya Kasa <kasatanya1@gmail.com>
2018-02-06 15:27:10 +0000

    role was added

--- auth.js
+++ auth.js
@@ -1,12 +1,13 @@
 var jwt = require('jsonwebtoken');
 var config = require('./config/config');
 
-
+// set user info from request
 exports.signToken = function(user){
     var jwtData = {
         id: user._id,
         firstname: user.firstname,
-        lastname: user.lastname
+        lastname: user.lastname,
+        role:user.role
     };
     return jwt.sign(
         {user: jwtData},
@@ -16,7 +17,7 @@ exports.signToken = function(user){
 
 exports.checkAuth = function(token){
     return new Promise(function(fulfill,reject){
-        console.log(token);
+       // console.log(token);
         jwt.verify(token, config.secret, function (err, user) {
             if(err)
                 reject(err);
