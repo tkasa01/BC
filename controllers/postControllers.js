4d49649d18b8e38cf5cb9e7b8bc0b0e34e756464
Tanya Kasa <kasatanya1@gmail.com>
2018-02-10 22:33:53 +0000

    get for review

--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -1,8 +1,10 @@
 /**
  * Created by tkasa on 07/01/2018.
  */
+
 var mongoose = require('mongoose');
 var Post = require('../models/Post');
+var Review = require('../models/Post');
 var Customer = require('../models/Customer');
 var async = require('async');
 
@@ -10,42 +12,42 @@ var postController = {};
 
 postController.list = function (req, res, next) {
     Post.find({}).populate('author').exec(function (err, posts) {
-        if(err){
-            res.send(err);
-        }else{
-            var list = [];
-            async.forEach(posts,function(post,callback){
-                if(post.author_id){
-                    Customer.findById(post.author_id,function(err,customer){
-                        console.log(customer)
-                        list.push({
-                            post: post,
-                            author: customer
-                        });
+            if(err){
+                res.send(err);
+            }else{
+                var list = [];
+                async.forEach(posts,function(post,callback){
+                    if(post.author_id){
+                        Customer.findById(post.author_id,function(err,customer){
+                            //console.log(customer);
+                            list.push({
+                                post: post,
+                                author: customer
+                            });
+                            callback();
+                        })
+                    }else{
                         callback();
-                    })
-                }else{
-                    callback();
-                }
-            },function(err){
-                if(err) res.send(err);
-                res.render('./posts/posts', {
-                    pageTitle: 'Recent posts from customers',
-                    posts:  list,
-                    user: req.user
+                    }
+                },function(err){
+                    if(err) res.send(err);
+                   // var dob = req.dob.toISOString();
+                   // dob = dob.substring(0, dob.indexOf('T'));
+                    res.render('./posts/posts', {
+                        pageTitle: 'Recent posts from customers',
+                        posts:  list,
+                        user: req.user
+                    });
                 });
-            });
-
-
+            }
         }
-    }
     );
 };
 
 postController.savePost = function(req, res, next) {
 
     if(req.user){
-        console.log(req.user.id)
+        //console.log(req.user.id);
         var post = new Post({
             title:req.body.title,
             content: req.body.content,
@@ -94,6 +96,63 @@ postController.params = function(req, res, next, id){
     })
 };
 
+//====================== REVIEWS =================================
+postController.saveReview = function(req, res, next) {
+    if(req.user){
+        console.log(req.user.id);
+        var review = new Review({
+            title:req.body.title,
+            description: req.body.description,
+            rating: req.body.rating,
+            author: author._id,
+            created:req.body.created,
+            builder : req.builder
+           // author_id: req.user.id
+        });
+        console.log(review);
+        review.save(function (err, reviews) {
+            if (err) {
+                res.send(err);
+            } else {
+                console.log("saved" + reviews);
+                res.redirect('/post/postReview' + req.params.id);
+            }
+        });
+    }else{
+        res.send('You are not logged in');
+    }
+
+};
+
+
+/*
+ Post.findById(author).populate('author').exec(function (err, author) {
+ if (err) {
+ res.send(err);
+ }
+ else {
+ // console.log("saved" + post);
+ res.redirect('/posts/posts' + author);
+ }
+ });
+ */
+/*
+postController.params = function(req, res, next, id){
+    Post.findById(id).populate('author', 'email').exec().then(function(err, post){
+        if(!post){
+            next(new Error('No post found from this id'));
+        }else{
+            post.title = req.body.title;
+            post.content = req.body.content;
+            post.update = req.body.update;
+            post.author = req.body.author;
+            req.post = post;
+            next();
+        }
+    },function(err){
+        next(err);
+    })
+};*/
 /*
 postController.update = function (req, res) {
         var post = req.post;
