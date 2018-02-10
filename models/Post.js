efe2180b0f3054a5f0722a7c1f224ec66d8f464d
Tanya Kasa <kasatanya1@gmail.com>
2018-02-10 22:35:14 +0000

    review schema

--- models/Post.js
+++ models/Post.js
@@ -2,31 +2,42 @@ const mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var PostSchema = new Schema({
-    //_id: mongoose.Schema.Types.ObjectId,
     title:{ type:String, required: true},
     content: {type: String, required: true},
-    author_id:{
-        type:String,
-        required: true
-    },
+    author_id:[{
+        type:Schema.Types.ObjectId,
+        ref: 'Customer'
+    }],
+   // author_id:{
+   //     type:String,
+   //     required: true
+  //  },
     timestamp: {type: Date},
     update: {type: Date, default: Date.now()}
 
 });
-    /*,
-   builder:{
-        type: String,
-        default: null //if !null then its a review
-    },
-    review:[{
-        title: String,
-        description: String,
-        rating: Number,
-        created: {
-        type: Date,
-        default: Date.now
-        }]
-    }*/
+
+var ReviewSchema = new Schema({
+         builder:{
+             type: Schema.Types.ObjectId, //String,
+             ref: 'Builder',
+             default: null //if !null then its a review
+         },
+
+        author_id:{type:Schema.Types.ObjectId,
+             ref:'Customer'
+        },
+         review:[{
+             title: String,
+             description: String,
+             rating: Number,
+             created: {
+                     type: Date,
+                     default: Date.now
+              }
+          }]
+
+});
 
 
 
@@ -34,7 +45,7 @@ var PostSchema = new Schema({
 //this middleware make sure that this post is created from this author
 
 PostSchema.pre('save', function (next) {
-    author  = Post(this.author); //slygify in the video
+    author_id  = Post(this.author_id); //slygify in the video
     next();
 });
 
@@ -61,3 +72,4 @@ function post(text){
 
 var Post = module.exports = mongoose.model('Post', PostSchema);
 
+var Review = module.exports = mongoose.model('Review', ReviewSchema);
