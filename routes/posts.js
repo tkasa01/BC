928afa4f446bbcb6b68b13bc62851a2cf06960d2
Tanya Kasa <kasatanya1@gmail.com>
2018-02-14 20:56:37 +0000

    review

--- routes/posts.js
+++ routes/posts.js
@@ -4,21 +4,14 @@
 var express = require('express');
 var postRouter = express.Router();
 var posts = require('../controllers/postControllers');
-var customers = require('../controllers/customersControllers');
 
 postRouter.post('/posts',  posts.savePost);
 
 postRouter.get('/posts', posts.list);
 
-postRouter.get('/postReview',  function (req, res, next) {
-    var id = req.params.id;
-    res.redirect(req.params.id + 'postReview');
- ;
-    next();
-});
+postRouter.post('/postReview/:id', posts.saveReview) ;
 
-postRouter.post('/customers/profile/:id/postReview', posts.saveReview);
-
+postRouter.get('/builders/profile/:id', posts.listReviews);
 
 
 
