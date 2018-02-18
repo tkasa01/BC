eb367b10790ef048e9f6e6502a2069ba8c48b941
Tanya Kasa <kasatanya1@gmail.com>
2018-02-18 22:05:20 +0000

    trying to display review on builders' profile

--- routes/posts.js
+++ routes/posts.js
@@ -9,19 +9,30 @@ postRouter.post('/posts',  posts.savePost);
 
 postRouter.get('/posts', posts.list);
 
+postRouter.post('/delete/:id',  posts.checkOwner, posts.delete); //
+
+
 postRouter.post('/postReview/:id', posts.saveReview) ;
 
-postRouter.get('/builders/profile/:id', posts.listReviews);
+//postRouter.post('/delete/:id', loggedIn, posts.delete);
 
+//postRouter.get('/builders/profile/:id', posts.listReviews);
+
+postRouter.get('/reviews/:id', posts.listReviews);
 
 
 /*
- postRouter.post( '/posts',posts.savePost);
 postRouter.get('/:id',posts.getOne);
 postRouter.put('/:id',posts.update);
-postRouter.delete('/:id',posts.deletePost);
-*/
 
+*/
+function loggedIn(req, res, next) {
+    if(req.user){
+        next();
+    }else{
+        res.redirect('/login');
+    }
+}
 module.exports = postRouter;
 
 
