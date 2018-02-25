f0d420861d7a40df817a74e9c454a07fda81698d
Tanya Kasa <kasatanya1@gmail.com>
2018-02-25 16:35:57 +0000

    trying to display review on builders' profile

--- middlewareApp.js
+++ middlewareApp.js
@@ -10,6 +10,12 @@ module.exports = function (app) {
     app.use(bodyParser.urlencoded({ extended: true }));
     app.use(bodyParser.json());
     app.use(cookieParser());
+    app.use(function(req, res, next){
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+        res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        next();
+    });
 
 
     //for jwt token
