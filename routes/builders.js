ecd95013efd0dfcd64a929984c98eefc31200c3b
Tanya Kasa <kasatanya1@gmail.com>
2018-03-09 15:09:03 +0000

    multer and function for upload images was added

--- routes/builders.js
+++ routes/builders.js
@@ -5,6 +5,20 @@ var express = require('express');
 var router = express.Router();
 var builders = require('../controllers/buildersControllers');
 
+var multer = require('multer');
+
+
+var storage = multer.diskStorage({
+    destination: function(req, file, cb){
+        cb(null,'./public/uploads/')
+    },
+    filename: function(req, file, cb){
+        cb(null, Date.now() + file.originalname);
+
+    }
+});
+var upload = multer({ dest: './public/uploads/', storage: storage});
+
 router.get('/list', builders.list);
 
 router.get('/registration',builders.showRegistrationPage);
@@ -25,8 +39,12 @@ router.post('/update/:id', builders.update);
 
 router.post('/delete/:id', builders.delete);
 
-//router.post('/postReview/:id', builders.postReview);
+router.post('/profile/:id/upload', upload.single('img'), function(req, res){
+    console.log('helloooooooo images');
+    res.json({file: req.file});
+});
 
+//router.post('/postReview/:id', builders.postReview);
 //router.get('/postReview/:id',builders.displayPost);
 
 module.exports = router;
